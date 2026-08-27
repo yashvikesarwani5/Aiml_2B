fee3cc479980a5e51efb2867590727cef234c701
Yashvi <yashvikesarwani5@gmail.com>
2026-08-27 12:06:30 +0530

    question

---
+++ obj.js
@@ -0,0 +1,29 @@
+let student = {
+    name: "Yashvi",
+    lastName: "Kesharwani",
+    city:"Alopibagh",
+    pincode:211006,
+    salary:function(){
+        document.write("Salary is 50000");
+    },
+    subjects:["Maths","Java","C++"]
+};
+console.log(student.name);
+console.log(student.lastName);
+console.log(student.city);
+console.log(student.pincode);
+console.log(student.subjects);
+student.salary();
+
+llet student = {
+    name: "Yashvi",
+    lastName: "Kesharwani",
+    city: "Alopibagh",
+    pincode: 211006,
+
+    salary: function () {
+        document.write("Salary is 50000");
+    },
+
+    subjects: ["Maths", "Java", "C++"]
+};
